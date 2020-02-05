ba6c39685a7f877655bf9201d58865b9a44a30ba
Shubham Gupta <32667635+Shubham1204@users.noreply.github.com>
2020-02-05 21:31:10 +0530

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const staticDevCoffee = "dev-coffee-site-v1";
+const staticCoffee = "coffee-site-v1";
 const assets = [
   "/",
   "/index.html",
@@ -17,7 +17,7 @@ const assets = [
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(staticCoffee).then(cache => {
       cache.addAll(assets);
     })
   );
